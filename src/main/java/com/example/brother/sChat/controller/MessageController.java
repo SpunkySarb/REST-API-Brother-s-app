138ea8d88ca9f8a745141bf21cd0d05cd861c4be
Sarb <59626972+SpunkySarb@users.noreply.github.com>
2020-12-23 08:22:19 -0500

    Update MessageController.java

--- src/main/java/com/example/brother/sChat/controller/MessageController.java
+++ src/main/java/com/example/brother/sChat/controller/MessageController.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-
+import org.springframework.web.servlet.ModelAndView;
 import com.example.brother.sChat.dao.MessageDao;
 import com.example.brother.sChat.model.Data;
 import com.example.brother.sChat.model.MessageModel;
